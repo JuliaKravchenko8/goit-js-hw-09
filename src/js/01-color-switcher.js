fdcbee080009a6812b55ab7c50cc56a20777b004
JuliaKravchenko8 <107355879+JuliaKravchenko8@users.noreply.github.com>
2022-11-21 23:42:29 +0200

    update task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,22 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let intervalId = null;
+
+// startBtn.addEventListener('click', onStartClick);
+// stopBtn.addEventListener('click', onStopClick);
+
+startBtn.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(intervalId);
+  stopBtn.disabled = false;
+});
