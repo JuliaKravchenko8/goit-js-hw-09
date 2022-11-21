54a72fc51193e9225680ec16d663eb5c4e9889ef
JuliaKravchenko8 <107355879+JuliaKravchenko8@users.noreply.github.com>
2022-11-21 21:42:55 +0000

    Deploying to gh-pages from @ JuliaKravchenko8/goit-js-hw-09@fdcbee080009a6812b55ab7c50cc56a20777b004 🚀

---
+++ 01-color-switcher.08cc653f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let a=null;e.addEventListener("click",(()=>{a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,e.disabled=!0}),1e3)})),t.addEventListener("click",(()=>{clearInterval(a),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.08cc653f.js.map
